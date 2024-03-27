fdd7f089dce24f9732615f7b2e39307fa41ec7f2
JherlinsonVillaneda <jherlinson10@gmail.com>
2024-03-27 15:14:02 -0500

    first commit

---
+++ src/main/ExerciseOne.java
@@ -0,0 +1,8 @@
+package main;
+
+public class ExerciseOne {
+    public static void main(String[] args) {
+        //Print Hello first and then in another line my name.
+        System.out.println("Helo\nJherlinson Villaneda");
+    }
+}
---
+++ src/main/ExerciseTwo.java
@@ -0,0 +1,12 @@
+package main;
+
+import java.util.Scanner;
+
+public class ExerciseTwo {
+    public static void main(String[] args) {
+        int num1 = 20;
+        int num2 = 30;
+        int sum = num2 + num1;
+        System.out.println(sum);
+    }
+}
